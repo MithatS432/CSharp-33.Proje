2d2a930abd3fcfc27b307fa83e7eef92153b5cdb
Mithat <mithatsirmali@gmail.com>
2025-05-29 15:39:38 +0300

    Add files via upload

---
+++ Hata Yönetimi1/Hata Yönetimi1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hata_Yönetimi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Bir sayı girin: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi < 0)
+                {
+                    throw new Exception("Negatif sayı giremezsiniz!");
+                }
+
+                Console.WriteLine($"Girdiğiniz sayı: {sayi}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
